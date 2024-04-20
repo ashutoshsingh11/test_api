35b91893830bd249a2531d7514a87caec46f88df
ashutoshsingh11 <singhashutosh609@gmail.com>
2024-04-20 20:58:38 +0530

    Update index.js
    second commit

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const app  = express()
 
 app.listen(5001,() => console.log('Api is Running  '))
 
-app.get('/',(req,res) => res.json('My Api running at 5001 ') )
+app.get('/',(req,res) => res.json('My Api running at 5001 Miau Miau') )
